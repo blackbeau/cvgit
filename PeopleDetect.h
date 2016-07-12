0020541cfa458d4bfad67525dc28e655d7448219
YK <ykaiwan@163.com>
2016-07-13 05:42:24 +0800

    PDinterface

---
+++ PeopleDetect.h
@@ -0,0 +1,25 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+using namespace cv;
+class PeopleDetect{
+
+public:
+	PeopleDetect()
+	{
+		hog.setSVMDetector(hog.getDefaultPeopleDetector());
+	}
+	void Detect(const Mat& src, vector<Rect>& found,
+				Size maxsize,Size minsize){
+		hog.detectMultiScale(src, found);
+		vector<Rect> foundtemp;
+		for (auto r : found){
+			if (maxsize.area() > r.area() && minsize.area() < r.area())
+				foundtemp.push_back(r);
+		}
+		found = foundtemp;
+	}
+private:
+	HOGDescriptor hog;
+};
+int main(){}
